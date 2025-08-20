624af1072dd56bbb90208022b21a941d1fc377b0
muho <mh.jung.dev@gmail.com>
2025-08-20 23:36:30 +0900

    reinit

---
+++ service/hotel-service/src/test/java/multi/module/hotel/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package multi.module.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
